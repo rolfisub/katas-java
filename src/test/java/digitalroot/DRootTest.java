5de16210205cd24041c3d596a631e96abf8394d6
Rolf Bansbach <rolfisub@gmail.com>
2019-09-08 23:37:51 -0400

    added target and ran tests

--- src/test/java/digitalroot/DRootTest.java
+++ src/test/java/digitalroot/DRootTest.java
@@ -9,5 +9,6 @@ public class DRootTest {
     public void Tests() {
         assertEquals("Nope!", 7, DRoot.digital_root(16));
         assertEquals("Nope!", 6, DRoot.digital_root(456));
+        assertEquals("Nope!", 5, DRoot.digital_root(431231126));
     }
 }
