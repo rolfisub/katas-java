199863ebc690ebd92a7063be8da567a6dafb8a04
Rolf Bansbach <rolfisub@gmail.com>
2019-09-08 23:32:46 -0400

    added junit and tests

---
+++ src/test/java/digitalroot/DRootTest.java
@@ -0,0 +1,13 @@
+package digitalroot;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DRootTest {
+
+    @Test
+    public void Tests() {
+        assertEquals("Nope!", 7, DRoot.digital_root(16));
+        assertEquals("Nope!", 6, DRoot.digital_root(456));
+    }
+}
