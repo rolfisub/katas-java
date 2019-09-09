ddcb99ed78323743534c22d23cc68f3f30f8ce57
Rolf Bansbach <rolfisub@gmail.com>
2019-09-08 23:14:13 -0400

    first kata digital root

---
+++ src/main/java/digitalroot/DRoot.java
@@ -0,0 +1,18 @@
+package digitalroot;
+
+public class DRoot {
+    public static int digital_root(int number)
+    {
+        String numberAsString = String.valueOf(number);
+        if(numberAsString.length() > 1) {
+            int total = 0;
+            char[] eachChar = numberAsString.toCharArray();
+            for(char x: eachChar) {
+                total += Character.getNumericValue(x);
+            }
+            return DRoot.digital_root(total);
+        } else {
+            return number;
+        }
+    }
+}
